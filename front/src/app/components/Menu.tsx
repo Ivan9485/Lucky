f21b8cf3a250798470ebf265110fcecbde7c99a7
Ivan <pascual_815@hotmail.com>
2024-04-22 10:08:49 -0600

    feat: Menu First draft

--- front/src/app/components/Menu.tsx
+++ front/src/app/components/Menu.tsx
@@ -1,3 +1,39 @@
 import inventory from "@/app/data/inventory.json"
 
 // componente
+
+const Menu = () => {
+  return (
+    <div className=" w-full h-56 bg-white ">  
+      {/* Favorites Section */}
+      <div className=" flex flex-col">
+        {/* Title */}
+        <div className="text-lg"> Favoritos </div>
+        {/* Grid */}
+        <div className="grid-flow-row">
+        {
+          inventory.map((item, index)=>{
+            return (
+              <div key={index}>
+                <div className="text-lg">{item.name}</div>
+                <div className="text-sm">{item.description}</div>
+                <div className="text-sm">{item.price}</div>
+              </div>
+            )
+          }) 
+        }
+        </div>
+      </div>
+      {/* Whole menu Section*/}
+      <div className="">
+        {/* Tabs */}
+        <div></div>
+        {/* Grid */}
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Menu
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
+import Menu from "../app/components/Menu"
 
 export default function Home() {
   return (
-   <div className="h-screen w-full bg-green-200 justify-center flex flex-col">
-    <div className="text-center ">
+   <div className="h-screen w-full bg-green-200 flex flex-col">
+    <div className="text-center my-10 ">
       <h1 className="">
         Lucky bakery :3
       </h1>
+    </div>
+    <div className="">
+    <Menu />
     </div>
    </div>
   );
